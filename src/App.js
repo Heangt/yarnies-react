dedd384d6bbae641ac77aa9fcbf1841c246c9327
Heang <116808016+Heangt@users.noreply.github.com>
2023-12-22 21:36:28 +0100

    Add content to the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-
+import { Yarnies } from "./Yarnies.jsx";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Yarnies />
     </div>
   );
 }
---
+++ src/Yarnies.jsx
@@ -0,0 +1,53 @@
+export const Yarnies = () => {
+  return (
+    <div>
+      <div class="section">
+        <div class="container1">
+          <h1> FREE to join for all Yarn Lovers!!</h1>
+          <p>
+            {" "}
+            Have you heard about this community, Yarn lovers? It's a vibrant,
+            welcome home for all things crochet and knitting.
+          </p>
+          <button> Join community</button>
+        </div>
+        <img
+          alt="Yarnies"
+          src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/045/719/original/yarn.jpeg?1662574040"
+        />
+      </div>
+      <div class="container2">
+        <h2>What is the Yarn Lovers Community?</h2>
+        <p>
+          At Yarn lovers, we've created a space where you can dive into crochet
+          and knitting by learning from one another, whether you're a
+          <em>
+            <span class="importance1"> beginner or a pro.</span>
+          </em>
+          We are crocheters and knitters, just like you, and we've created this
+          <em>
+            <span class="importance1"> free community </span>
+          </em>
+          for you to join. Start learning from each other,
+          <em>
+            <span class="importance1"> tips, tricks, and inspiration </span>
+          </em>{" "}
+          from the Yarn lovers community!
+        </p>
+        <hr />
+        <p>
+          If you have any question, you contact us:
+          <a
+            href="https://www.google.com/gmail/about/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <strong>
+              <span class="importance2">hello@yarnlovers.com</span>
+            </strong>
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
